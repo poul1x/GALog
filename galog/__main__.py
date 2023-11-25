4d5bc48b31f70355606109bdd425f4ed484fc3a8
poul1x <poulix.nova@mail.ru>
2023-11-25 23:53:05 +0300

    Remove logging

--- galog/__main__.py
+++ galog/__main__.py
@@ -5,9 +5,4 @@ import yaml
 from galog.app import runApp
 
 if __name__ == "__main__":
-    # Configure logging
-    with open("logging.yaml") as f:
-        dictConfig(yaml.safe_load(f))
-
-    # Run application
     runApp()
