e30e8c7d094d2a29e75e8af1dfbc6a06298998f7
Pavel Knyazev <poulix.nova@mail.ru>
2025-02-16 04:24:07 +0300

    Refactor tag filter dialog

---
+++ galog/app/ui/core/tag_filter_dialog/control_button_bar.py
@@ -0,0 +1,25 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QWidget
+from galog.app.ui.base.widget import BaseWidget
+
+
+class ControlButtonBar(BaseWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+        self.initUserInterface()
+
+    def initUserInterface(self):
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.setContentsMargins(10, 0, 10, 0)
+        self.addTagButton = QPushButton("Add", self)
+        self.removeTagButton = QPushButton("Remove", self)
+        self.removeAllTagsButton = QPushButton("Remove all", self)
+        self.saveToFileButton = QPushButton("Save to file", self)
+        self.loadFromFileButton = QPushButton("Load from file", self)
+        vBoxLayout.addWidget(self.addTagButton)
+        vBoxLayout.addWidget(self.removeTagButton)
+        vBoxLayout.addWidget(self.removeAllTagsButton)
+        vBoxLayout.addWidget(self.saveToFileButton)
+        vBoxLayout.addWidget(self.loadFromFileButton)
+        vBoxLayout.setAlignment(Qt.AlignTop)
+        self.setLayout(vBoxLayout)
---
+++ galog/app/ui/core/tag_filter_dialog/filtered_tags_list.py
@@ -0,0 +1,79 @@
+from typing import List
+
+from PyQt5.QtCore import QModelIndex, Qt, pyqtSignal
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QAbstractItemView, QListView, QVBoxLayout, QWidget
+
+from galog.app.ui.base.list_view import ListView
+from galog.app.ui.base.widget import BaseWidget
+
+
+class FilteredTagsList(BaseWidget):
+    selectionChanged = pyqtSignal()
+
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+        self.initUserInterface()
+
+    def initUserInterface(self):
+        vBoxLayout = QVBoxLayout()
+        vBoxLayout.setAlignment(Qt.AlignTop)
+        vBoxLayout.setContentsMargins(0, 0, 0, 0)
+        vBoxLayout.setSpacing(0)
+
+        self.listView = ListView(self)
+        self.listView.setEditTriggers(QListView.NoEditTriggers)
+        self.listView.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        self.dataModel = QStandardItemModel(self)
+        self.listView.setModel(self.dataModel)
+        vBoxLayout.addWidget(self.listView)
+        self.setLayout(vBoxLayout)
+
+        # Make it easier to subscribe for this signal in parent widget
+        self.listView.selectionModel().selectionChanged.connect(
+            self.selectionChanged.emit
+        )
+
+    def _selectedRows(self):
+        def key(index: QModelIndex):
+            return index.row()
+
+        selectionModel = self.listView.selectionModel()
+        selectedRows = sorted(selectionModel.selectedRows(), key=key, reverse=True)
+        return [index.row() for index in selectedRows]
+
+    def toStringList(self) -> List[str]:
+        result = []
+        for index in range(self.dataModel.rowCount()):
+            item = self.dataModel.item(index)
+            result.append(item.text())
+
+        return result
+
+    def hasTag(self, tag: str):
+        return bool(self.dataModel.findItems(tag))
+
+    def addTag(self, tag: str):
+        self.dataModel.appendRow(QStandardItem(tag))
+
+    def addManyTags(self, tags: List[str]):
+        for tag in tags:
+            self.dataModel.appendRow(QStandardItem(tag))
+
+    def setTags(self, tags: List[str]):
+        self.removeAllTags()
+        self.addManyTags(tags)
+
+    def hasTags(self):
+        return self.dataModel.rowCount() > 0
+
+    def hasSelectedTags(self):
+        return bool(self.listView.selectionModel().selectedRows())
+
+    def removeSelectedTags(self):
+        for row in self._selectedRows():
+            self.dataModel.removeRow(row)
+
+    def removeAllTags(self):
+        self.dataModel.clear()
